8ed02b38b016077f007227dc78b63ec7b7e49981
Kenneth <obikennethe913@gmail.com>
2022-08-21 10:49:50 +0200

    bcrypt added to hash user password

--- src/schemas/authors.js
+++ src/schemas/authors.js
@@ -6,10 +6,13 @@ const authorsSchema = new Schema(
     name: { type: String, required: true },
     email: { type: String, required: true },
     avatar: { type: String, required: true },
+    password: {type: String, required: true},
+    role: {type: String, enum: ["User", "Admin"], default: "User"}
   },
   {
     timestamps: true,
   }
 )
+
 
 export default authorsSchema
--- src/server.js
+++ src/server.js
@@ -4,16 +4,30 @@ import cors from "cors"
 import listEndpoints from "express-list-endpoints"
 import blogRoutes from "./services/blogs/routes.js"
 import authorRouter from "./services/authors/routes.js"
+import {forbiddenHandler, unauthorizedHandler, catchAllHandler} from "./errorHandlers.js"
+
 const server = express()
 
 const port = process.env.PORT || 3001
 server.use(express.json())
 server.use(cors())
 
+
+// **************** ENDPOINTS ***************
 server.use("/api/blogPosts", blogRoutes)
 server.use("/api/authors", authorRouter)
 
+
+
+
+//************* ERROR HANDLERS *************
+server.use(unauthorizedHandler)
+server.use(forbiddenHandler)
+server.use(catchAllHandler)
+
+
 mongoose.connect(process.env.MONGODB_URI)
+
 
 mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB")
